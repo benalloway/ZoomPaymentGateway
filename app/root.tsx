75c9992be530b9148f4971cfba4e9c7305ffdf0b
Benjamin Alloway <Benjamin_Alloway@carmax.com>
2024-08-14 14:30:06 -0400

    layout edits, add menu

--- app/root.tsx
+++ app/root.tsx
@@ -4,13 +4,23 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLocation,
 } from "@remix-run/react";
 import "./tailwind.css";
 import { StackedLayout } from "./components/StackedLayout";
-import { Navbar } from "./components/Navbar";
+import {
+  Navbar,
+  NavbarDivider,
+  NavbarItem,
+  NavbarSection,
+} from "./components/Navbar";
 import { Sidebar } from "./components/Sidebar";
+import { Avatar } from "./components/Avatar";
+import { Link } from "./components/Link";
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const location = useLocation();
+
   return (
     <html lang="en">
       <head>
@@ -20,7 +30,29 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <StackedLayout navbar={<Navbar></Navbar>} sidebar={<Sidebar></Sidebar>}>
+        <StackedLayout
+          navbar={
+            <Navbar>
+              <Link href="/">
+                <Avatar className="size-10" src="/public/favicon.ico" />
+              </Link>
+              <NavbarDivider />
+              <NavbarSection>
+                <NavbarItem
+                  className={
+                    location.pathname.includes("webinars")
+                      ? "border-b-2 border-gray-500"
+                      : ""
+                  }
+                  href="/webinars"
+                >
+                  Webinars
+                </NavbarItem>
+              </NavbarSection>
+            </Navbar>
+          }
+          sidebar={<Sidebar></Sidebar>}
+        >
           {children}
         </StackedLayout>
         <ScrollRestoration />
