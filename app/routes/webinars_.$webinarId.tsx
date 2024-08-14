16bb2878141ed02bb606adf2248f9b9c7ef2045c
Benjamin Alloway <Benjamin_Alloway@carmax.com>
2024-08-14 10:47:10 -0400

    webinar details

--- app/routes/webinars_.$webinarId.tsx
+++ app/routes/webinars_.$webinarId.tsx
@@ -1,6 +1,10 @@
-import { json, LoaderFunctionArgs } from "@remix-run/cloudflare";
-import { Form, useLoaderData } from "@remix-run/react";
-import { FormEvent } from "react";
+import {
+  ActionFunctionArgs,
+  json,
+  LoaderFunctionArgs,
+} from "@remix-run/cloudflare";
+import { Form, useActionData, useLoaderData } from "@remix-run/react";
+import { ExclamationCircleIcon } from "@heroicons/react/24/solid";
 
 import { Button } from "~/components/Button";
 import { Divider } from "~/components/Divider";
@@ -11,7 +15,7 @@ import {
   Label,
   Legend,
 } from "~/components/Fieldset";
-import { Heading } from "~/components/Heading";
+import { Heading, Subheading } from "~/components/Heading";
 import { Input } from "~/components/Input";
 import { Text } from "~/components/Text";
 import {
@@ -19,46 +23,92 @@ import {
   getWebinar,
   WebinarDetail,
 } from "~/services/zoom/zoomServices";
+import {
+  DescriptionDetails,
+  DescriptionList,
+  DescriptionTerm,
+} from "~/components/DescriptionList";
 
-export async function loader({ params }: LoaderFunctionArgs) {
+export const loader = async ({ params }: LoaderFunctionArgs) => {
   const accessToken = await getAccessToken();
   const webinar: WebinarDetail = await getWebinar(
     accessToken,
     params.webinarId!
   );
   return json(webinar);
-}
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+
+  const data = {
+    first_name: formData.get("first_name") as string,
+    last_name: formData.get("last_name") as string,
+    email: formData.get("email") as string,
+    credit_card_number: formData.get("credit_card_number") as string,
+    credit_card_expiration: formData.get("credit_card_expiration") as string,
+    credit_card_cvc: formData.get("credit_card_cvc") as string,
+  };
+
+  console.log(data);
+
+  // authorize and charge card on authorizeNet
+
+  const chargeRes = false;
+
+  // if not successfully charged, display error returned from authorizeNet
+  // - return json({errorMessage})
+  if (!chargeRes) return json({ errorMessage: "unable to charge card" });
+
+  // if successfully charged, register for zoom webinar
+  // return redirect("/webinars/registration-confirmation", {
+  //     headers: {
+  //         ""
+  //     }
+  // })
+
+  // If cannot successfully register, display "we successfully charged your card, please reach out for us to register you for event"
+  // - return json({errorMessage})
+
+  // if successfully registered, redirect to confirmation page and display results of registration (webinar date, join url, add-to-calendar, etc.)
+  // - make sure to retry a few times
+
+  return null;
+};
 
 export default function Webinar() {
   const webinar: WebinarDetail = useLoaderData();
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-
-    const data = {
-      first_name: formData.get("first_name") as string,
-      last_name: formData.get("last_name") as string,
-      email: formData.get("email") as string,
-      credit_card_number: formData.get("credit_card_number") as string,
-      credit_card_expiration: formData.get("credit_card_expiration") as string,
-      credit_card_security_code: formData.get(
-        "credit_card_security_code"
-      ) as string,
-    };
-
-    console.log(data);
-  };
+  const actionData = useActionData<typeof action>();
 
   return (
     <div>
-      <Heading>Register for webinar</Heading>
-      <section className="mt-10">
-        <Form action="" onSubmit={handleSubmit}>
-          <Fieldset>
-            <Legend>Webinar Details</Legend>
-            <Text>{webinar.topic ?? "[No topic is set for this webinar]"}</Text>
+      <Subheading level={2}>Register for webinar</Subheading>
+      <DescriptionList>
+        <DescriptionTerm>Topic</DescriptionTerm>
+        <DescriptionDetails>
+          {webinar.topic ?? "[No topic set for this webinar]"}
+        </DescriptionDetails>
 
+        <DescriptionTerm>Agenda</DescriptionTerm>
+        <DescriptionDetails>
+          {webinar.agenda.trim()
+            ? webinar.agenda
+            : "[No agenda set for this webinar]"}
+        </DescriptionDetails>
+
+        <DescriptionTerm>Agenda</DescriptionTerm>
+        <DescriptionDetails>
+          {webinar.duration
+            ? webinar.duration + " minutes"
+            : "[No agenda set for this webinar]"}
+        </DescriptionDetails>
+
+        <DescriptionTerm>Price</DescriptionTerm>
+        <DescriptionDetails>$14.99</DescriptionDetails>
+      </DescriptionList>
+      <section className="mt-10">
+        <Form method="post">
+          <Fieldset>
             <FieldGroup>
               <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-4">
                 <Field>
@@ -117,9 +167,10 @@ export default function Webinar() {
                     />
                   </Field>
                   <Field>
-                    <Label>Security Code</Label>
+                    <Label>CVC</Label>
                     <Input
-                      name="credit_card_security_code"
+                      className="max-w-[6rem]"
+                      name="credit_card_cvc"
                       autoComplete="cc-csc"
                       type="number"
                       pattern="\d{3,4}"
@@ -131,6 +182,15 @@ export default function Webinar() {
                 </div>
               </div>
               <Divider />
+
+              {actionData?.errorMessage?.trim() && (
+                <div className="flex flex-row space-x-4 border-l-4 border-red-500 bg-red-100 h-16 items-center rounded-sm">
+                  {/* <ExclamationCircleIcon className="text-red-600 ml-2" height={48} /> */}
+                  <Text className="ml-4 !text-red-600 text-center">
+                    {actionData.errorMessage}
+                  </Text>
+                </div>
+              )}
 
               <Button type="submit" className="w-full">
                 Register
